7b4f2a7287b0ae39918cd0ecbab265bb788536c5
davified <davified@gmail.com>
2018-06-18 11:14:46 +0800

    add solutions

---
+++ src/InputForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class InputForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ""
+    };
+  }
+  render() {
+    return (
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type="text"
+          value={this.state.inputValue}
+          onChange={event => this.handleChange(event)}
+        />
+        <button>Add name</button>
+      </form>
+    );
+  }
+
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addNewName(this.state.inputValue);
+
+    this.setState({
+      inputValue: ""
+    });
+  }
+}
+
+export default InputForm;
